8ca62e2b945167a5142933a590b7463590a8d5a3
sarav <saraverdiani@hotmail.com>
2024-09-03 16:52:20 +0200

    Aggiunta metodi per corretto funzionamento algoritmo A*

--- MapSearchNode.cpp
+++ MapSearchNode.cpp
@@ -3,3 +3,162 @@
 //
 
 #include "MapSearchNode.h"
+
+
+const int MAP_WIDTH = 20;
+const int MAP_HEIGHT = 20;
+
+int world_map[ MAP_WIDTH * MAP_HEIGHT ] =
+{
+
+	// 0001020304050607080910111213141516171819
+	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,   // 00
+	1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,1,   // 01
+	1,9,9,1,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 02
+	1,9,9,1,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 03
+	1,9,1,9,1,9,9,9,1,9,1,9,1,1,1,1,9,9,1,1,   // 04
+	1,9,1,1,9,1,1,1,1,9,1,1,1,1,9,1,1,1,1,1,   // 05
+	1,9,9,9,9,1,1,1,1,1,1,9,9,9,9,1,1,1,1,1,   // 06
+	1,9,9,9,9,9,9,9,9,1,1,1,9,9,9,9,9,9,9,1,   // 07
+	1,9,1,1,1,1,1,1,1,1,1,9,1,1,1,1,1,1,1,1,   // 08
+	1,9,1,9,9,9,9,9,9,9,1,1,9,9,9,9,9,9,9,1,   // 09
+	1,9,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,1,1,   // 10
+	1,9,9,9,9,9,1,9,1,9,1,9,9,9,9,9,1,1,1,1,   // 11
+	1,9,1,9,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 12
+	1,9,1,9,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 13
+	1,9,1,1,1,1,9,9,1,9,1,9,1,1,1,1,9,9,1,1,   // 14
+	1,9,1,1,9,1,1,1,1,9,1,1,1,1,9,1,1,1,1,1,   // 15
+	1,9,9,9,9,1,1,1,1,1,1,9,9,9,9,1,1,1,1,1,   // 16
+	1,1,9,9,9,9,9,9,9,1,1,1,9,9,9,1,9,9,9,9,   // 17
+	1,9,1,1,1,1,1,1,1,1,1,9,1,1,1,1,1,1,1,1,   // 18
+	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,   // 19
+
+};
+
+
+
+
+
+
+int GetMap( int x, int y )
+{
+	if( x < 0 ||
+	    x >= MAP_WIDTH ||
+		 y < 0 ||
+		 y >= MAP_HEIGHT
+	  )
+	{
+		return 9;
+	}
+
+	return world_map[(y*MAP_WIDTH)+x];
+}
+
+bool MapSearchNode::IsSameState( MapSearchNode &rhs )
+{
+
+
+	if( (x == rhs.x) &&
+		(y == rhs.y) )
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+size_t MapSearchNode::Hash()
+{
+	size_t h1 = hash<float>{}(x);
+	size_t h2 = hash<float>{}(y);
+	return h1 ^ (h2 << 1);
+}
+
+void MapSearchNode::PrintNodeInfo()
+{
+  const int strSize = 100;
+	char str[strSize];
+	snprintf( str, strSize, "Node position : (%d,%d)\n", x,y );
+
+	cout << str;
+}
+
+
+float MapSearchNode::GoalDistanceEstimate( MapSearchNode &nodeGoal )
+{
+	return abs(x - nodeGoal.x) + abs(y - nodeGoal.y);
+}
+
+bool MapSearchNode::IsGoal( MapSearchNode &nodeGoal )
+{
+
+	if( (x == nodeGoal.x) &&
+		(y == nodeGoal.y) )
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool MapSearchNode::GetSuccessors( AStarSearch<MapSearchNode> *astarsearch, MapSearchNode *parent_node )
+{
+
+	int parent_x = -1;
+	int parent_y = -1;
+
+	if( parent_node )
+	{
+		parent_x = parent_node->x;
+		parent_y = parent_node->y;
+	}
+
+
+	MapSearchNode NewNode;
+
+
+	if( (GetMap( x-1, y ) < 9)
+		&& !((parent_x == x-1) && (parent_y == y))
+	  )
+	{
+		NewNode = MapSearchNode( x-1, y );
+		astarsearch->AddSuccessor( NewNode );
+	}
+
+	if( (GetMap( x, y-1 ) < 9)
+		&& !((parent_x == x) && (parent_y == y-1))
+	  )
+	{
+		NewNode = MapSearchNode( x, y-1 );
+		astarsearch->AddSuccessor( NewNode );
+	}
+
+	if( (GetMap( x+1, y ) < 9)
+		&& !((parent_x == x+1) && (parent_y == y))
+	  )
+	{
+		NewNode = MapSearchNode( x+1, y );
+		astarsearch->AddSuccessor( NewNode );
+	}
+
+
+	if( (GetMap( x, y+1 ) < 9)
+		&& !((parent_x == x) && (parent_y == y+1))
+		)
+	{
+		NewNode = MapSearchNode( x, y+1 );
+		astarsearch->AddSuccessor( NewNode );
+	}
+
+	return true;
+}
+
+float MapSearchNode::GetCost( MapSearchNode &successor )
+{
+	return (float) GetMap( x, y );
+
+}
+
--- MapSearchNode.h
+++ MapSearchNode.h
@@ -5,9 +5,28 @@
 #ifndef MAPSEARCHNODE_H
 #define MAPSEARCHNODE_H
 
+#include "stlastar.h"
+
 
 
 class MapSearchNode {
+
+public:
+
+    int x;	 // the (x,y) positions of the node
+    int y;
+
+    MapSearchNode() { x = y = 0; }
+    MapSearchNode( int px, int py ) { x=px; y=py; }
+
+    float GoalDistanceEstimate( MapSearchNode &nodeGoal );
+    bool IsGoal( MapSearchNode &nodeGoal );
+    bool GetSuccessors( AStarSearch<MapSearchNode> *astarsearch, MapSearchNode *parent_node );
+    float GetCost( MapSearchNode &successor );
+    bool IsSameState( MapSearchNode &rhs );
+    size_t Hash();
+
+    void PrintNodeInfo();
 
 
 
